e1a0f44fa63b990ed26c33c598142466bba3ea39
Christopher Thauer <thauerc@gmail.com>
2022-01-10 09:53:24 -0800

    Completed project

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,11 @@
+import spinner from '../assets/loading.gif';
+
+const Spinner = () => (
+  <img
+    src={spinner}
+    alt="..."
+    style={{width: '100px', margin: 'auto', display: 'block'}}
+  />
+)
+
+export default Spinner;
